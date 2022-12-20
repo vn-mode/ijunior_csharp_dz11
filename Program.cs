d57cd29063be7a5a7d31352913f04bedf33a0484
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2022-12-20 19:01:16 +0300

    ijunior_csharp_dz11

--- Program.cs
+++ Program.cs
@@ -9,26 +9,31 @@ namespace vn_mode_csharp_dz11
             int number;
             int stepNumber1 = 3;
             int stepNumber2 = 5;
-            int sumOfNumbersDivisibleByThree = 0;
-            int sumOfNumbersDivisibleByFive = 0;
+            int minNumber = 3;
+            int maxNumber = 101;
+            int sumOfNumbersDivisibleByStepNumber1 = 0;
+            int sumOfNumbersDivisibleByStepNumber2 = 0;
             int finalSum;
 
-            Random rand = new Random();
-            number = rand.Next(3, 101);
+            Random random = new Random();
+            number = random.Next(minNumber, maxNumber);
 
             for (int i = stepNumber1; i <= number; i += stepNumber1)
             {
-                sumOfNumbersDivisibleByThree += i;
+                if ((i % stepNumber2) != 0)
+                {
+                    sumOfNumbersDivisibleByStepNumber1 += i;
+                }
             }
             for (int i = stepNumber2; i <= number; i += stepNumber2)
             {
-                sumOfNumbersDivisibleByFive += i;
+                sumOfNumbersDivisibleByStepNumber2 += i;
             }
 
-            finalSum = sumOfNumbersDivisibleByThree + sumOfNumbersDivisibleByFive;
+            finalSum = sumOfNumbersDivisibleByStepNumber1 + sumOfNumbersDivisibleByStepNumber2;
             Console.WriteLine($"Рандомное чисно = {number}.");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Cумма всех положительных чисел меньше {number} (включая число), которые кратные 3 или 5 = {finalSum}.");
+            Console.WriteLine($"Cумма всех положительных чисел меньше {number} (включая число), которые кратные {stepNumber1} или {stepNumber2} = {finalSum}.");
 
         }
     }
